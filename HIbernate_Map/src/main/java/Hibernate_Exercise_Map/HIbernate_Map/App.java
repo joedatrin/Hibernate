0ecba22d69c2016a23435a97c81472a5177e9d98
JOE DATRIN <55043835+joedatrin@users.noreply.github.com>
2022-05-02 11:59:32 +0530

    Update App.java

--- HIbernate_Map/src/main/java/Hibernate_Exercise_Map/HIbernate_Map/App.java
+++ HIbernate_Map/src/main/java/Hibernate_Exercise_Map/HIbernate_Map/App.java
@@ -16,6 +16,7 @@ public class App
         SessionFactory sessionFactory=configuration.buildSessionFactory();
         Session session=sessionFactory.openSession();
         
+     //Adding First Student Details
         Student student1=new Student();
         Address address=new Address();
         address.setAddress_id(14);
@@ -23,9 +24,21 @@ public class App
         student1.setId(3);
         student1.setName("abc");
         student1.setAddress(address);
+     
        session.beginTransaction();
        session.save(student1);
        session.getTransaction().commit();
+     //Adding Second Student Details
+        address2.setAddress_id(201);
+        address2.setStreet("2Thirty Six");
+        student2.setId(205);
+        student2.setName("2abc");
+        student2.setAddress(address2);
+        
+        session.beginTransaction();
+        session.save(student2);
+        session.getTransaction().commit();
+        
        //session.close();
     }
 }
